70806ffa19b9b0020a25adac5b4114526efd58e1
wuchengqi <wuchengqi577@gmail.com>
2021-01-12 11:32:39 +0800

    高德地图源替换修改谷歌地图源

--- src/redux/init/lyrs.js
+++ src/redux/init/lyrs.js
@@ -7,22 +7,20 @@ import vector from 'images/map/vector.png'
 const vectorLayer = new ol.layer.Tile({
   source: new ol.source.XYZ({
     crossOrigin: 'anonymous',
-    url: 'http://www.google.cn/maps/vt?lyrs=m@733&gl=cn&x={x}&y={y}&z={z}'
+    url: 'http://wprd0{1-4}.is.autonavi.com/appmaptile?x={x}&y={y}&z={z}&lang=zh_cn&size=1&scl=1&style=7'
   })
 })
 const imageLayer = new ol.layer.Tile({
   source: new ol.source.XYZ({
     crossOrigin: 'anonymous',
-    url: 'http://www.google.cn/maps/vt?lyrs=s@718&gl=cn&x={x}&y={y}&z={z}'
+    url: 'http://wprd0{1-4}.is.autonavi.com/appmaptile?x={x}&y={y}&z={z}&lang=zh_cn&size=1&scl=2&style=6'
   })
-    
 })
 const textLayer = new ol.layer.Tile({
   source: new ol.source.XYZ({
     crossOrigin: 'anonymous',
-    url: 'http://www.google.cn/maps/vt?lyrs=h@733&gl=cn&x={x}&y={y}&z={z}'
+    url: 'http://wprd0{1-4}.is.autonavi.com/appmaptile?x={x}&y={y}&z={z}&lang=zh_cn&size=1&scl=1&style=8'
   })
-    
 })
 
 export default [
